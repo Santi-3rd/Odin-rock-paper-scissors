74305e5e92c2699282d30851e26806be80426c91
Guillermo Santiago <g.santiago3rd@gmail.com>
2024-01-23 11:45:41 -0700

    built most of logic in js file, need to limit the score to 5

--- script.js
+++ script.js
@@ -1,23 +1,66 @@
+
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
     let choices = ["rock", "paper", "scissors"];
     let randomizer = Math.floor(Math.random() * choices.length);
+    // let computerChoice = choices[randomizer]
     return choices[randomizer]
 }
 
-function getPlayerChoice(){ //This should listen for button presses 
-        let rock = document.querySelector('#rock')
-        rock.addEventListener('click', function(event){
-            console.log(event.type)
-        })
-    }
+// function getPlayerChoice(){ //This should listen for button presses 
+        let playerChoice = ''
 
-function playRound(){
+        let rock = document.querySelector('#rock')
+        rock.onclick = function(){
+            playerChoice = 'rock'
+            playRound(playerChoice)
+        }
+        let paper = document.querySelector('#paper')
+        paper.onclick = function(){
+            playerChoice = 'paper'
+            playRound(playerChoice)
+        }
+        let scissors = document.querySelector('#scissors')
+        scissors.onclick = function(){
+            playerChoice = 'scissors'
+            playRound(playerChoice)
+        }
+
+function playRound(playerChoice){
     
+    console.log(playerChoice)
+    let computerChoice = getComputerChoice()
+    console.log(computerChoice)
+
+    roundWinner = ''
+    if (playerChoice == 'rock' && computerChoice == 'scissors') {
+        roundWinner = 'p'
+    }
+    else if (playerChoice == 'scissors' && computerChoice == 'rock') {
+        roundWinner = 'p'
+    }
+    else if (playerChoice == 'rock' && computerChoice == 'scissors') {
+        roundWinner = 'p'
+    }
+    else {
+        roundWinner = 'c'
+    }
+    
+    game(roundWinner)
 
 }
 
-function game(){
+function game(roundWinner){
 
-    let playerScore = 0;
-    let computerScore = 0;
+
+    if (roundWinner == 'p'){
+        playerScore += 1
+    }
+    else {
+        computerScore += 1
+    }
+    console.log(playerScore, computerScore)
+    
 }
